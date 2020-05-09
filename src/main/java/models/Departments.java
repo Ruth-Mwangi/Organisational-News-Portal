241ffc6b63313299edcbed67b2a656ae1ee0cf5c
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-05-09 07:41:29 +0300

    Update Departments.java with variables getter and setters and constructor

--- src/main/java/models/Departments.java
+++ src/main/java/models/Departments.java
@@ -1,4 +1,40 @@
 package models;
 
+import java.util.Objects;
+
 public class Departments {
+
+    private String name;
+    private int id;
+
+    public Departments(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Departments that = (Departments) o;
+        return id == that.id &&
+                Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, id);
+    }
 }
