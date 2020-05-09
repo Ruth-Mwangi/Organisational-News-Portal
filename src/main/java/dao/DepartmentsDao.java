83ac94eebfa341bd8f3c15dc95700eb45ceb6fc9
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-05-09 07:47:33 +0300

    Added crud methods in DepartmentsDao

--- src/main/java/dao/DepartmentsDao.java
+++ src/main/java/dao/DepartmentsDao.java
@@ -1,4 +1,21 @@
 package dao;
 
+import models.Departments;
+import models.Users;
+
+import java.util.List;
+
 public interface DepartmentsDao {
+
+    //create
+    void add();
+    void addUserToDepartment(Users user, Departments department);
+    //read
+
+    List<Departments> getAll();
+    Departments findById(int id);
+    List<Users> getAllUsersInDepartment(int id);
+    //update
+    //delete
+
 }
